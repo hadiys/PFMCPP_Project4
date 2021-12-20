0a426e13efbf821cdfcbff3cef6b794d5f8a8417
Hadi Saleh <66527346+hadiys@users.noreply.github.com>
2021-12-20 20:40:04 +0000

    Correction: fixed indentation

--- main.cpp
+++ main.cpp
@@ -93,36 +93,27 @@ good to go!
  
  #include <iostream>
 
- struct FloatType
- {
+struct FloatType
+{
     float add(float lhs, float rhs);
-
     float subtract(float lhs, float rhs);
-
     float multiply(float lhs, float rhs);
-
     float divide(float lhs, float rhs);
- };
+};
 
- struct DoubleType
- {
+struct DoubleType
+{
     double add(double lhs, double rhs);
-
     double subtract(double lhs, double rhs);
-
     double multiply(double lhs, double rhs);
-
     double divide(double lhs, double rhs);
- };
+};
 
 struct IntType
 {
     int add(int lhs, int rhs);
-
     int subtract(int lhs, int rhs);
-
     int multiply(int lhs, int rhs);
-
     int divide(int lhs, int rhs);
 };
 
